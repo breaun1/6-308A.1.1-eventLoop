4eec330948cecde33cfa834e9b4fe34e96bb3426
breaun1 <breaun1@gmail.com>
2023-12-27 19:04:38 -0600

    Got the counter running, and stacked over 20k calls...

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let counter = 0;
 
 function reCount() {
+    //without the conditional the counter will run/stack over 20k times... wow
     if (counter >= 10) {
         console.log("Recursion has reached the maximum value.");
         return;
